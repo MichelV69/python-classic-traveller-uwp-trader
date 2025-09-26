6366b14c226d5549c4ab4fb45a379c6ea37c1203
Michel @ Mac Mini Intel <michel@wolfstar.ca>
2025-09-26 15:55:45 -0300

    First pass at simple output genertion.

--- app.py
+++ app.py
@@ -1,75 +1,38 @@
-import random
 from classicTravellerRules import *
+from diceBag import *
 
-# --- functions
-
-
-def d6(rolls):
-    result = 0
-    done = 0
-    while done < rolls:
-        done += 1
-        result += random.randint(1, 6)
-    return result
-
-
-def actualValue(twoDsix):
-    if twoDsix < 2:
-        twoDsix = 2
-    if twoDsix > 15:
-        twoDsix = 15
-
-    match twoDsix:
-        case 2:
-            result = 0.4
-        case 3:
-            result = 0.5
-        case 4:
-            result = 0.7
-        case 5:
-            result = 0.8
-        case 6:
-            result = 0.9
-        case 7:
-            result = 1.0
-        case 8:
-            result = 1.1
-        case 9:
-            result = 1.2
-        case 10:
-            result = 1.3
-        case 11:
-            result = 1.5
-        case 12:
-            result = 1.7
-        case 13:
-            result = 2.0
-        case 14:
-            result = 3.0
-        case 15:
-            result = 4.0
-
-    return result
 
 # --- main work
 
 
 def main():
 
-    src = Planet("sourceworld", "x-000000-0")
-    src.uwp = input("\n Please enter the Source Planet's UWP (x-123456-7): ")
-    print("got " + src.uwp)
+    src = Planet("sourceworld", "C-888888-C")
+    # src.uwp = input("\n Please enter the Source Planet's UWP (x-123456-7): ")
 
-    dest = Planet("destworld", "x-000000-0")
-    dest.uwp = input("\n Please enter the Destination Planet's UWP (x-123456-7): ")
-    print("got " + dest.uwp)
+    dest = Planet("destworld", "A-456789-B")
+    # dest.uwp = input("\n Please enter the Destination Planet's UWP (x-123456-7): ")
 
-    brokerLevel = input("\n Please enter the available Broker level (0 .. 4): ")
-    print("brokerLevel: " + brokerLevel)
+    brokerLevel = (
+        1  # input("\n Please enter the available Broker level (0 .. 4): ")
+    )
 
-    # roll a pair of D6, do not add them together.
+    # roll a pair of D6, do not add them together.[[
     d6a = random.randint(1, 6)
     d6b = random.randint(1, 6)
+    # ]]
+
+    # weird dice mod stuff from pp.104 [[
+    if src.population() >= 9:
+        d6a += 1
+    if src.population() <= 5:
+        d6a -= 1
+
+    if d6a < 1:
+        d6a == 1
+    if d6a > 6:
+        d6a == 1
+    # ]]
 
     dicePair = d6a * 10 + d6b
     print("dicePair: " + str(dicePair))
--- classicTravellerRules.py
+++ classicTravellerRules.py
@@ -1,8 +1,12 @@
+from diceBag import *
+
 # --- classes
 
 
 class Planet:
-    uwp_map = dict(starport=0, size=2, atmo=3, hydro=4, pop=5, gov=6, law=7, tech=9)
+    uwp_map = dict(
+        starport=0, size=2, atmo=3, hydro=4, pop=5, gov=6, law=7, tech=9
+    )
 
     def __init__(self, name, uwp):
         self.name = name
@@ -61,6 +65,23 @@ class Planet:
             return False
         return True
 
+    def is_industrial(self):
+        if (
+            self.atmosphere() > 2
+            and self.atmosphere() != 4
+            and self.atmosphere() != 7
+            and self.atmosphere() != 9
+        ):
+            return False
+        if self.population() < 9:
+            return False
+        return True
+
+    def is_nonIndustrial(self):
+        if self.population() > 6:
+            return False
+        return True
+
     def atmosphere(self):
         return self.hexToInt(self.uwp[self.uwp_map["atmo"]])
 
@@ -72,6 +93,7 @@ class Planet:
 
     def government(self):
         return self.hexToInt(self.uwp[self.uwp_map["gov"]])
+
 
 # --- functions
 
@@ -101,23 +123,33 @@ def textiles(src, dest, brokerLevel):
     if src.is_nonIndustrial():
         valueRoll -= 3
 
+    indent = "\n    "
     purchaseModPercent = actualValue(valueRoll)
-    brokerCommission = getBrokerCommission(brokerLevel)
-
     localCost = basePrice * purchaseModPercent
+    orderValue = availableQuantity * localCost
+    brokerCommission = round(
+        float(getBrokerCommission(brokerLevel)) * int(orderValue), 0
+    )
 
-    resultText = "Cargo Available: " + availableQuantity + " tonnes "
-    resultText += " with a common market value of " + basePrice + "."
-    resultText += "\n    We can purchase them locally for Cr" + localCost
-    resultText += ", which is a valuation "
+    resultText = "Cargo Available: " + str(availableQuantity) + " tonnes "
+    resultText += " with a common market value of " + str(basePrice) + "."
+    resultText += indent + "We can purchase them locally for Cr"
+    resultText += str(localCost) + " per tonne, which is a valuation "
     if purchaseModPercent < 1:
         resultText += "below"
     else:
         resultText += "at or above"
-    resultText += " common market value."
-    if brokerLevel > 0:
-        resultText += "This is in part due to our Broker, who will charge us"
-        resultText += " Cr" + brokerCommission + " for this transaction."
+    resultText += (
+        " common market value." + indent + "That will cost us a total of Cr"
+    )
+    resultText += str(orderValue) + "."
+    if int(brokerLevel) > 0:
+        resultText += (
+            indent + "This is in part due to our Broker, who will charge us"
+        )
+        resultText += (
+            " Cr" + str(brokerCommission) + " for this transaction."
+        )
 
     valueRoll = d6(2)
     if dest.is_agricultural():
