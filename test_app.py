bb5d5a14470797eae79d58c3ebe68c01d22713fe
Michel @ Mac Mini Intel <michel@wolfstar.ca>
2025-09-26 18:17:01 -0300

    Improved test set and coverage

--- test_app.py
+++ test_app.py
@@ -1,6 +1,9 @@
 import pytest
 from classicTravellerRules import *
 
+RI_AG_UWP = "C-767795-9"
+NAG_NIN_P_UWP = "B-222699-B"
+
 
 class TestGetBrokerCommission:
     def test_brokerLvl1(self):
@@ -17,7 +20,8 @@ class TestGetBrokerCommission:
 
 
 class TestPlanetMethods:
-    blueMarble = Planet("BlooSpec", "B-678567-C")
+    blueMarble = Planet("BlooSpec", RI_AG_UWP)
+    brownRock = Planet("Crumplli", NAG_NIN_P_UWP)
 
     def test_HexToInt(self):
         assert self.blueMarble.hexToInt("A") == 10
@@ -25,20 +29,23 @@ class TestPlanetMethods:
         assert self.blueMarble.hexToInt("F") == 15
 
     def test_extractStatAtmosphere(self):
-        assert self.blueMarble.atmosphere() == 7
+        assert self.blueMarble.atmosphere() == 6
 
     def test_extractStatGovernment(self):
-        assert self.blueMarble.government() == 6
+        assert self.blueMarble.government() == 9
 
     def test_isAgWorld(self):
         assert self.blueMarble.is_agricultural()
-        assert not (self.blueMarble.is_nonAgricultural())
+        assert self.brownRock.is_nonAgricultural()
 
     def test_isRichWorld(self):
-        assert not self.blueMarble.is_rich()
-        assert not self.blueMarble.is_poor()
+        assert self.blueMarble.is_rich()
+        assert self.brownRock.is_poor()
 
     def test_getTradeClasses(self):
+        assert self.blueMarble.getTradeClasses().__contains__("[Ri]")
         assert self.blueMarble.getTradeClasses().__contains__("[Ag]")
-        assert not (self.blueMarble.getTradeClasses().__contains__("[Ind]"))
-        assert self.blueMarble.getTradeClasses().__contains__("[NIn]")
+
+        assert self.brownRock.getTradeClasses().__contains__("[NAg]")
+        assert self.brownRock.getTradeClasses().__contains__("[NIn]")
+        assert self.brownRock.getTradeClasses().__contains__("[P]")
